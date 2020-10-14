ed35d1508c52c6a45440b8a85ad2379e3a90e1dc
alexeybliznyuk <66327586+alexeybliznyuk@users.noreply.github.com>
2020-10-14 22:14:20 +0300

    0.1 fix

--- knb.py
+++ knb.py
@@ -3,39 +3,39 @@ import random
 
 
 start_my_turn = input("Chouse your turn(stone(0),scissors(1), paper(2)")
-str(start_en_turn = random.randint(0,2))
+start_en_turn = str(random.randint(0,2))
 if start_en_turn == "0":
 	if start_my_turn == "0":
 		end = 'Ничья'
-		break
+		
 	elif start_my_turn == '1':
 	 	end = 'Defeat'
-	 	break
+	 	
 	elif start_my_turn == '2':
 	 	end = "Win"
-	 	break
+	 	
 	 
 elif start_en_turn == '1':
 	 if start_my_turn == '0':
 	 	end = 'Win'
-	 	break
+	 	
 	 elif start_my_turn == '1':
 	 	end = 'Ничья'
-	 	break
+	 	
 	 elif start_my_turn == '2':
 	 	end = 'Defeat'
-	 	break
+	 	
 	 
 elif start_en_turn == "2":
 	 if start_my_turn == '0':
 	 	end = "Defeat"
-	 	break
+	 	
 	 elif start_my_turn == '1':
 	 	end = 'Win'
-	 	break
+	 	
 	 elif start_my_turn == '2':
 	 	end = 'Ничья'
-	 	break
+	 	
 	 
 	 		
-print(f'{end}')
+print(end)
