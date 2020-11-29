2154abdc23fd6f77e60cde169a6e91177d9d3ec8
alexeybliznyuk <66327586+alexeybliznyuk@users.noreply.github.com>
2020-11-29 15:46:23 +0300

    change int turn

---
+++ print.py
@@ -0,0 +1,32 @@
+import random
+
+
+
+
+start_my_turn = int(input("Chouse your turn(stone(0),scissors(1), paper(2) :"))
+start_en_turn = random.randint(0, 2)
+if start_en_turn == 0:
+    if start_my_turn == 0:
+        end = 'Ничья'
+    elif start_my_turn == 1:
+        end = 'Defeat'
+    elif start_my_turn == 2:
+        end = "Win" 
+
+elif start_en_turn == 1:
+    if start_my_turn == 0:
+        end = 'Win' 
+    elif start_my_turn == 1:   
+        end = 'Ничья' 
+    elif start_my_turn == 2:
+        end = 'Defeat'
+
+elif start_en_turn == 2:
+    if start_my_turn == 0:
+        end = "Defeat" 
+    elif start_my_turn == 1:
+        end = 'Win' 
+    elif start_my_turn == 2: 
+        end = 'Ничья'
+
+print(f'{end}')
